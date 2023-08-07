e78a7c068cbca1f4ccb359031690cbb25eeacebe
Roberto Junior <robertojuniorazul@gmail.com>
2023-08-07 11:03:37 -0300

    Configurando o Next Auth

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,50 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+// import { api } from '../../lib/axios'
+import { Container, Header } from '../styles'
+import { ConnectBox, ConnectItem } from './styles'
+import { signIn, useSession } from 'next-auth/react'
+
+
+export default function Register() {
+  const session = useSession()
+
+  // async function handleRegister() {
+    
+  // }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua Agenda</Heading>
+        <Text>
+        Conecte o seu calendário para verificar automaticamente as horas ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm" onClick={()=> signIn('google')}>
+            Conectar
+            <ArrowRight />
+          </Button>
+        </ConnectItem>
+
+        <pre>
+          <Text>{JSON.stringify(session.data)}</Text>
+        </pre>
+
+        <Button type="submit">
+            Próximo passo
+            <ArrowRight />
+        </Button>
+      </ConnectBox>
+
+      
+     
+    </Container>
+  )
+}
